35efc5eed33ecdd729986f726e0764b933a8dbee
BattleBaxx <54268057+Ranga1503@users.noreply.github.com>
2020-06-15 12:06:16 +0530

    changed main

--- Client/main.cpp
+++ Client/main.cpp
@@ -1,8 +1,21 @@
-#include <QCoreApplication>
-
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include "client.h"
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 
-    return a.exec();
+    QGuiApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+    const QUrl url(QStringLiteral("qrc:/main.qml"));
+    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
+                     &app, [url](QObject *obj, const QUrl &objUrl) {
+        if (!obj && url == objUrl)
+            QCoreApplication::exit(-1);
+    }, Qt::QueuedConnection);
+    engine.load(url);
+    client test;
+
+    return app.exec();
 }
