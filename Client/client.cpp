3d27652ea3f9ca714a17dd8ec248f799d0d10bde
BattleBaxx <54268057+Ranga1503@users.noreply.github.com>
2020-06-26 13:00:59 +0530

    added whiles

--- Client/client.cpp
+++ Client/client.cpp
@@ -6,7 +6,6 @@ using namespace std;
 
 client::client(QObject* parent):QObject(parent)
 {
-    qDebug() << "Constructor\n";
     socket = new QTcpSocket(this);
     connect(socket, SIGNAL(readyRead()), this, SLOT(readyRead()));
     socket->connectToHost("localhost", 9999);
@@ -19,13 +18,23 @@ void client::readyRead()
     qDebug() << "Reading\n";
     //QString str;
     char buf[1024] = {0};
-         //str = QTextCodec::codecForMib(1015)->toUnicode(socket->readLine());
-         socket->read(buf, sizeof(buf));
-         qDebug() << buf;
-       // cin >> buf;
-       socket->write("buf");
-       socket->flush();
+    //str = QTextCodec::codecForMib(1015)->toUnicode(socket->readLine());
+    socket->read(buf, sizeof(buf));
+    cout << buf;
+    while(buf[0] != '!')
+    {
+        cout << "\n\nEnter the message: ";
+        cin >> buf;
+        cin.sync();
+        socket->write(buf);
+        socket->flush();
+        socket->waitForBytesWritten(30000);
 
-    qDebug("\nDone");
+        socket->waitForReadyRead();
+        socket->read(buf, sizeof(buf));
+        cout << "Message sent by Server is: " << buf << endl;
+    }
+
+    qDebug("\nCompleted");
 
 }
--- Server/server.cpp
+++ Server/server.cpp
@@ -22,17 +22,25 @@ void server::newConnection()
     qDebug() <<  "Connected";
     // need to grab the socket
     QTcpSocket *socket = ser->nextPendingConnection();
+    char* buf = new char [1024];
+
     socket->write("Hello client this is the QTcpServer on port 9999");
     socket->flush();
-
     socket->waitForBytesWritten(30000);
-    qDebug() <<  "Write done";
-    socket->waitForReadyRead();
-    char* buf = new char [1024];
-    //str = QTextCodec::codecForMib(1015)->toUnicode(socket->readLine());
-    socket->read(buf, sizeof(buf));
-    qDebug() << buf;
+    while(buf[0] != '!')
+    {
+        socket->waitForReadyRead();
+        socket->read(buf, sizeof(buf));
+        cout << "\n\nMessage sent by Client is: " << buf;
 
+        cout << "\n\nEnter the message: ";
+        cin >> buf;
+        cin.sync();
+        socket->write(buf);
+        socket->flush();
+        socket->waitForBytesWritten(30000);
+    }
+    cout << "\n\nClosing the socket";
 
     socket->close();
 }
