b892911e1beba794840081908bff6170fe3d6326
subhasrim2001 <58735732+subhasrim2001@users.noreply.github.com>
2020-07-24 22:41:14 +0530

    Skip and reverse class play functions

--- Server/reverse.cpp
+++ Server/reverse.cpp
@@ -8,7 +8,17 @@ Reverse::Reverse(int value, char color) : Special (value)
 
 string Reverse :: play(Game &game, int choice, char ch)
 {
-    game.top_card = game.player.card_list[choice-1];
-    game.player.card_list.erase(game.player.card_list.begin()+choice-1);
-    return "Check";
+    if(ch == 's')
+    {
+        vector<int>::iterator it;
+        it=find (game.player.card_list.begin(), game.player.card_list.end(), choice);
+        int n = it - game.player.card_list.begin();  //number from 0
+        game.player.card_list.erase(game.player.card_list.begin()+n);
+        /*Debugging code*/
+        /*for (int i=0; i<game.player.card_list.size(); i++)
+            std::cout << ' ' << game.player.card_list[i];*/
+        //returns the top card only
+    }
+    game.top_card = choice;
+    return "Play again";
 }
