c182bc44ef8f214169755cf60ab1adf6b835cddf
subhasrim2001 <58735732+subhasrim2001@users.noreply.github.com>
2020-07-25 10:47:47 +0530

    Update drawfour.h

--- Server/drawfour.h
+++ Server/drawfour.h
@@ -4,8 +4,9 @@
 #include "draw.h"
 #include <iostream>
 #include "game.h"
+#include"colorchange.h"
 using namespace std;
-class drawFour : public Draw
+class drawFour : public Draw, public colorChange
 {
 public:
     drawFour(int, int);
